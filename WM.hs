bb90af0e87d9025263fe771b9df3256138478e9f
Morita Yasuaki <zousandazou@gmail.com>
2013-10-22 13:20:46 +0900

    WM

--- WM.hs
+++ WM.hs
@@ -11,55 +11,110 @@ import Data.Bits.Lens
 import Data.Binary
 import Data.Word
 import Prelude as P
+import Data.List as L
 
 type N = Int
+
+bitW :: Int
+bitW = bitSize (-1::Int)
+
+-- |
+-- A[0..k)
+-- rank(v,q,i) = |{ j | A[j] = q, j ∈ [0..i) }|
+-- access(v,q,i) = min { j | A[j] = q, j ∈ [0..i) }
 
 class RSV v where
     type E v
     size :: v -> N
     access :: v -> N -> E v
-    rank :: v -> N -> E v -> N
-    select :: v -> N -> E v -> N
+    rank :: v -> E v -> N -> N
+    select :: v -> E v -> N -> N
 
 instance Eq a => RSV [a] where
     type E [a] = a
     size = P.length
     access = (!!)
-    rank xs n e = size . P.filter (== e) $ P.take n xs
-    select xs 0 e = 0
-    select xs n e =  fst (P.filter (\p -> snd p == e) (P.zip [0..] xs) !! (n-1))
+    rank xs e n = size . P.filter (== e) $ P.take n xs
+    select xs e n =  fst (P.filter (\p -> snd p == e) (P.zip [0..] xs) !! n)
+
+newtype BInt = BInt { btoi :: Int }
+instance RSV BInt where
+    type E BInt = Bool
+    size (BInt b) = bitW
+    access (BInt b) i = (b .&. (1 `unsafeShiftL` i)) == 1
+    rank = rankInt . btoi
+    select = selectInt . btoi
 
 
 -- |
--- >>> select (V.fromList [1,3,7,0xF] :: Vector Int) 8 True
+-- >>> rank (fromList [0x1F,0x30] :: Vector Int) True 69
+-- 6
 
 -- |
--- >>> rank (V.fromList [1,3,7,0xF] :: Vector Int) 194 True
---
+-- >>> select (fromList [0x1F,0x30] :: Vector Int) True 6
+-- 69
 
 instance RSV (Vector Int) where
     type E (Vector Int) = Bool
     size = V.length
     access v i = testBit ofs (v ! ind) 
-        where (ind,ofs) = i `divMod` (bitSize i) 
-    rank _ 0 _ = 0
-    rank v i True = ofscnt + V.sum (V.map popCount (V.unsafeSlice 0 ind v))
-        where ofscnt = popCount ((v ! ind) `unsafeShiftL` (bitSize i - ofs -1))
-              (ind,ofs) = (i-1) `divMod` bitSize i
-    rank v i False = i - (rank v i True)
-    select v i True = ind + ofs
-        where v1 = V.takeWhile (< i) vp
-              vp = prescanl' (+) 0 (V.map popCount v)
-              ind = (bitSize i) * (V.length v1 - 1)
-              ofs = select64 (v!(V.length v1 - 1)) (i - V.last v1 ) True
+        where (ind,ofs) = i `divMod` bitW 
+    rank v True i = ofscnt + V.sum (V.map popCount (V.unsafeSlice 0 ind v))
+        where ofscnt = rankInt (v ! ind) True ofs
+              (ind,ofs) = i `divMod` bitW
+    rank v False i = i - rank v True i
+    select v True i = bitW * ind + ofs
+        where vp = V.scanl' (+) 0 (V.map popCount v)
+              vf = V.takeWhile (< i) vp
+              ind = V.length vf - 1
+              ofs = selectInt (v!ind) True (i-vp!ind)
 
 -- |
--- >>> select64 8 1 True
+-- >>> rank ([1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0]::[Int]) 1 9
+-- 2
 
-select64 :: Int -> Int -> Bool -> Int
-select64 x 0 b = 0
-select64 x n b = 1 + fst (P.filter (f . snd)  [(s,testBit (unsafeShiftR x s) 0) | s <- [0..63]] !! (n-1))
-      where f = if b then id else not
+-- |
+-- >>> select ([1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0]::[Int]) 1 2
+-- 9
+
+-- |
+-- >>> rankInt 0x1F True 5
+-- 5
+
+-- |
+-- >>> selectInt 0x1F True 5
+-- 5
+
+rankInt :: Int -> Bool -> Int -> Int
+rankInt x True n = popCount (x .&. ((unsafeShiftL 1 n) - 1))
+rankInt x False n = bitW - rankInt x True n
+
+selectInt :: Int -> Bool -> Int -> Int
+selectInt x b n = ans
+    where f = if b then 0 else 1
+          cumsum :: [Int]
+          cumsum = P.scanl (+) 0 [f `xor` ((x `unsafeShiftR` s) .&. 1) | s <- [0..bitW-1]]
+          Just ans = L.findIndex (== n) cumsum
+
+popCumSum8 :: Int -> Int
+popCumSum8 s1 = let s2 = (s1 .&. 0x5555555555555555) +
+                         ((s1 .&. 0xAAAAAAAAAAAAAAAA) `unsafeShiftR` 1)
+                    s4 = (s2 .&. 0x3333333333333333) + 
+                         ((s2 .&. 0xCCCCCCCCCCCCCCCC) `unsafeShiftR` 2)
+                    s8 = (s4 .&. 0x0F0F0F0F0F0F0F0F) +
+                         ((s4 .&. 0xF0F0F0F0F0F0F0F0) `unsafeShiftR` 4)
+                    cs8 = s8 * 0x0101010101010101
+                 in cs8
+
+top8 :: Int -> Int
+top8 x = x `unsafeShiftR` (bitW - 8)
+
+-- |
+-- >>> popCount' 0x0103070F
+-- 10
+popCount' :: Int -> Int
+popCount' = top8 . popCumSum8
+
 
 main = pure ()
 
